5ce5b238f73de7d0e5b401c5a5115f6d00ec080b
Ximena Sarmiento <ximenatathis2705@gmail.com>
2024-11-17 18:32:17 -0500

    Agregué unas validaciones al registro

--- script.js
+++ script.js
@@ -8,4 +8,40 @@ document.getElementById('tipo_persona').addEventListener('change', function() {
         document.getElementById('campos_estudiante').style.display = 'block';
     }
 });
+/*
+document.getElementById("subirFotoBtn").addEventListener("click", function() {
+    const subirFotoBtn = document.getElementById('subirFotoBtn');
+    const fotoForm = document.getElementById('fotoForm');
+    
+    // Mostrar el formulario y ocultar el botón principal
+    subirFotoBtn.style.display = 'none';
+    fotoForm.style.display = 'block';
+});
 
+document.getElementById("fotoForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Evitar recargar la página
+
+    let formData = new FormData(this); // Crear el FormData con los datos del formulario
+    fetch("subir_foto.php", {
+        method: "POST",
+        body: formData
+    })
+    .then(response => response.json()) // Convertir la respuesta a JSON
+    .then(data => {
+        alert(data.message); // Mostrar el mensaje al usuario
+
+        if (data.status === "success") {
+            // Actualizar la imagen de perfil si la subida fue exitosa
+            document.querySelector('.foto-perfil img').src = 'imagenes/usuarios/' + document.getElementById('documentoUsuario').value + ".webp";
+        }
+
+        // Resetear el formulario y volver al estado inicial
+        this.reset();
+        document.getElementById('subirFotoBtn').style.display = 'block';
+        document.getElementById('fotoForm').style.display = 'none';
+    })
+    .catch(error => {
+        alert("Ocurrió un error al subir la foto.");
+    });
+});
+*/
